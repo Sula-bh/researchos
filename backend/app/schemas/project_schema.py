702b8d23af7aed8a7deb17fa6b9679cb5ba60844
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-29 15:19:57 +0530

    add schema for project response

--- backend/app/schemas/project_schema.py
+++ backend/app/schemas/project_schema.py
@@ -11,3 +11,13 @@ class ProjectCreateSchema(Schema):
         required=False,
         allow_none=True,
     )
+
+
+class ProjectResponseSchema(Schema):
+    id = fields.UUID()
+
+    title = fields.String()
+
+    description = fields.String(allow_none=True)
+
+    memory_dataset_id = fields.String(allow_none=True)
