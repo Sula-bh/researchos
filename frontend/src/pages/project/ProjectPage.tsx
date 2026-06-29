f448f6345e6bd99b99749cd820e327184028d8ad
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-29 22:04:32 +0530

    display each project

--- frontend/src/pages/project/ProjectPage.tsx
+++ frontend/src/pages/project/ProjectPage.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { getProject } from "@/api/projectApi";
 import ProjectSidebar from "./components/ProjectSidebar";
+import type { Project } from "@/types/project";
 
 export default function ProjectPage() {
+  const { projectId } = useParams();
+
+  const [project, setProject] = useState<Project>();
+
+  useEffect(() => {
+    if (!projectId) return;
+    const id = projectId;
+
+    async function fetchProject() {
+      try {
+        const data = await getProject(id);
+        setProject(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchProject();
+  }, [projectId]);
+
   return (
     <main className="flex h-screen">
       <ProjectSidebar />
 
       <section className="flex-1 p-8">
-        <h1>Project</h1>
+        <h1>{project?.title}</h1>
       </section>
     </main>
   );
