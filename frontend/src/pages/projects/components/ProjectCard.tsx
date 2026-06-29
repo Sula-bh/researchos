f3cc6385772bd1ad04f67439fcfd05d9fd524b48
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-29 21:34:52 +0530

    use card component in project card

--- frontend/src/pages/projects/components/ProjectCard.tsx
+++ frontend/src/pages/projects/components/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import type { Project } from "@/types/project";
 import { Link } from "react-router-dom";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 type ProjectCardProps = {
   project: Project;
 };
@@ -8,9 +10,15 @@ type ProjectCardProps = {
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <Link to={`/projects/${project.id}`}>
-      <h2>{project.title}</h2>
+      <Card className="mb-4 transition hover:shadow-lg">
+        <CardHeader>
+          <CardTitle>{project.title}</CardTitle>
+        </CardHeader>
 
-      <p>{project.description}</p>
+        <CardContent>
+          <p>{project.description}</p>
+        </CardContent>
+      </Card>
     </Link>
   );
 }
