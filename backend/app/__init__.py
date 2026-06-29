86473229b7ec4d68e6dc94e9ce930a8acbf1652c
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-29 11:23:09 +0530

    add extension for sql alchemy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 
 from app.config import Config
+from app.extensions import db
 
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
-    
+
+    db.init_app(app)
+
     @app.get("/")
     def home():
         return {"message": "ResearchOS API is running!"}
