0af213aae38e0ccad05bfe1c84b481e9573175d4
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-29 11:07:46 +0530

    add configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
+from app.config import Config
+
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object(Config)
+    
     @app.get("/")
     def home():
         return {"message": "ResearchOS API is running!"}
