d14f1d0cc593f79a007d2888a11e0c98560a7076
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-30 10:39:40 +0530

    add service for paper upload

--- backend/app/services/paper_service.py
+++ backend/app/services/paper_service.py
@@ -1,10 +1,13 @@
 from uuid import UUID
 
 from sqlalchemy import select
+from werkzeug.datastructures import FileStorage
 
 from app.exceptions.paper import PaperNotFoundError
 from app.extensions import db
 from app.models.paper import Paper
+from app.services.pdf_service import extract_metadata
+from app.services.storage_service import save_pdf
 
 
 def get_papers(project_id: UUID) -> list[Paper]:
@@ -31,3 +34,27 @@ def delete_paper(paper_id: UUID) -> None:
 
     db.session.delete(paper)
     db.session.commit()
+
+
+def upload_paper(project_id: UUID, file: FileStorage) -> Paper:
+
+    storage_key, file_name = save_pdf(
+        str(project_id),
+        file,
+    )
+
+    metadata = extract_metadata(storage_key)
+
+    paper = Paper(
+        project_id=project_id,
+        title=metadata["title"] or file_name,
+        authors=metadata["authors"] or None,
+        abstract=None,
+        file_name=file_name,
+        storage_key=storage_key,
+    )
+
+    db.session.add(paper)
+    db.session.commit()
+
+    return paper
