8287b3608a501d35f08edd8244f842eb7b30d8ce
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-06-30 10:26:35 +0530

    add endpoint for get paper

--- backend/app/routes/paper_routes.py
+++ backend/app/routes/paper_routes.py
@@ -19,6 +19,14 @@ def get_papers(project_id: UUID):
         data=paper_response_schema.dump(papers),
     )
 
+@paper_bp.get("/papers/<uuid:paper_id>")
+def get_paper(paper_id: UUID):
+    paper = paper_service.get_paper(paper_id)
+
+    return success_response(
+        data=single_paper_schema.dump(paper),
+    )
+
 @paper_bp.delete("/papers/<uuid:paper_id>")
 def delete_paper(paper_id: UUID):
     paper_service.delete_paper(paper_id)
