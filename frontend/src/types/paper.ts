38961ecd0e01e4a830e2193bd6d928a2b05e7b20
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-07-04 22:02:24 +0530

    add new fields

--- frontend/src/types/paper.ts
+++ frontend/src/types/paper.ts
@@ -1,10 +1,18 @@
 export interface Paper {
   id: string;
   project_id: string;
+
   title: string;
   authors: string | null;
   abstract: string | null;
+
   file_name: string;
   storage_key: string;
+
+  ai_status: string;
+  ai_summary: string | null;
+  ai_error: string | null;
+  processed_at: string | null;
+
   created_at: string;
 }
