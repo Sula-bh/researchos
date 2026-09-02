f433840462c00ea2563f321e3d00bb93aa673113
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-09-02 12:30:58 +0530

    wrap app with clerk provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ClerkProvider } from "@clerk/react";
+
 import "./index.css";
 import "@mdxeditor/editor/style.css";
 import App from "./App.tsx";
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 );
